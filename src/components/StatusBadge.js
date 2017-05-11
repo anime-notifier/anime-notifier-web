29b8752fd097488f4cbd8f4c46a22cbee1737b78
Vija02 <salim_michaelmi@yahoo.com>
2017-05-11 20:59:06 +0100

    feat: separate status badge into own component

---
+++ src/components/StatusBadge.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Badge } from 'reactstrap';
+
+class StatusBadge extends Component {
+  render() {
+    let child = <i className="fa fa-spin fa-spinner" />;
+    let color = "default";
+    switch(this.props.available){
+      case true:
+        color = "success";
+        child = "Available";
+        break;
+      case false:
+        color = "warning";
+        child = "Not Available";
+        break;
+      case "error":
+        child = "Unknown";
+        break;
+      default:
+        break;
+    }
+    return (
+      <Badge color={color}>{child}</Badge>
+    );
+  }
+}
+
+export default StatusBadge;
