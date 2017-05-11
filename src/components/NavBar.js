769ad756f5ddf70f9ce2d8c8358e35fccca429e3
Vija02 <salim_michaelmi@yahoo.com>
2017-05-11 20:38:55 +0100

    fix: dropdown responsive hack

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, NavbarBrand, Collapse, Nav, NavItem, NavLink, NavbarToggler, Dropdown, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Navbar, NavbarBrand, Collapse, Nav, NavItem, NavLink, NavbarToggler } from 'reactstrap';
 
 class NavBar extends Component {
   constructor(props){
@@ -17,16 +17,11 @@ class NavBar extends Component {
   render() {
     return (
       <Navbar light toggleable className="fixed-top" style={{backgroundColor: "rgba(0,0,0,.65)", position: "absolute"}}>
-        <NavbarToggler right tag="a" type="" onClick={() => {this.toggle()}} style={{cursor: "pointer"}}>
-          <Dropdown isOpen={this.state.isOpen} toggle={() => {}}>
-            <i className="fa fa-navicon" style={{color: "white", fontSize: "1.5em"}} />
-            <DropdownMenu right>
-              <DropdownItem disabled>About</DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
+        <NavbarToggler right onClick={() => {this.toggle()}}>
+          <i className="fa fa-navicon" style={{color: "white", fontSize: "1.5em"}} />
         </NavbarToggler>
         <NavbarBrand><Link to="/" style={{color: "white", textDecoration: 'none'}}>AniTrack</Link></NavbarBrand>
-        <Collapse navbar>
+        <Collapse navbar isOpen={this.state.isOpen}>
           <Nav className="ml-auto" navbar>
             <NavItem>
               <NavLink style={{color: "white"}}>About</NavLink>
