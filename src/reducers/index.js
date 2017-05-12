3e800f073e1072307be01fdc01df68069c695ba3
Vija02 <salim_michaelmi@yahoo.com>
2017-05-12 18:30:26 +0100

    feat: accept anime status by bulk

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,18 @@ import { routerReducer as routing } from 'react-router-redux';
 import { combineReducers } from 'redux';
 
 const anime = (state = { malAnimeList: [], animeList: {}}, action) => {
+  let list;
   switch (action.type) {
     case 'SET_ANIME_LIST':
       // Set an object with the title as the key
-      const list = {...state.animeList};
+      list = {...state.animeList};
       list[action.animeList.title] = action.animeList.available;
+      return {...state, animeList: list};
+    case 'SET_ANIME_LIST_BULK':
+      list = {...state.animeList};
+      action.animeListBulk.forEach((val) => {
+        list[val.title] = val.available;
+      })
       return {...state, animeList: list};
     case 'MAL_ANIME_LIST':
       return {...state, malAnimeList: action.malAnimeList};
