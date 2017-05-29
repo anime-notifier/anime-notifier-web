2b71de39e5c846c7084b3d84bd89a59698f57614
Vija02 <salim_michaelmi@yahoo.com>
2017-05-29 17:57:02 +0100

    refactor: change variable names, again..

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,37 +2,37 @@ import { routerReducer as routing } from 'react-router-redux';
 import { combineReducers } from 'redux';
 
 const anime = (state = { animeList: [], animeStatus: {}}, action) => {
-  let status;
+  let animeStatus;
   switch (action.type) {
     case 'RESET_ANIME_LIST':
       return {...state, animeList: []};
     case 'SET_ANIME_STATUS':
       // Set an object with the title as the key
-      status = {...state.animeStatus};
-      status[action.animeStatus.title] = action.animeStatus.available;
-      return {...state, animeStatus: status};
+      animeStatus = {...state.animeStatus};
+      animeStatus[action.animeStatus.title] = action.animeStatus.available;
+      return {...state, animeStatus: animeStatus};
     case 'SET_ANIME_STATUS_BULK':
-      status = {...state.animeStatus};
+      animeStatus = {...state.animeStatus};
       action.animeStatusBulk.forEach((val) => {
-        status[val.title] = val.available;
+        animeStatus[val.title] = val.available;
       })
-      return {...state, animeStatus: status};
+      return {...state, animeStatus: animeStatus};
     case 'ANIME_LIST':
       // If invalid, pass on the message
       if(action.animeList === "invalid_username"){
         return {...state, animeList: action.animeList};
       }
       // Handle finished and not aired anime status
-      status = {...state.animeStatus};
+      animeStatus = {...state.animeStatus};
       // Finished anime is always available
       action.animeList.filter(val => val.status === 2).forEach((val) => {
-        status[val.title] = true;
+        animeStatus[val.title] = true;
       })
       // Not aired anime is always not available
       action.animeList.filter(val => val.status === 3).forEach((val) => {
-        status[val.title] = false;
+        animeStatus[val.title] = false;
       })
-      return {...state, animeList: action.animeList, animeStatus: status};
+      return {...state, animeList: action.animeList, animeStatus: animeStatus};
     default:
       return state;
   }
