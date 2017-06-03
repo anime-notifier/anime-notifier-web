f9612c4915179f05570b12c509eba2d5cdf8f624
Vija02 <salim_michaelmi@yahoo.com>
2017-06-03 14:34:36 +0100

    fix: login button not pointer

--- src/containers/Login/css.js
+++ src/containers/Login/css.js
@@ -5,4 +5,4 @@ exports.card = {padding: "10px 15px 10px 15px", borderRadius: 0};
 exports.loginTitle = {textAlign: 'center', marginBottom: 20};
 exports.inputBox = {borderRadius: 0, backgroundColor: '#ededf5'};
 
-exports.loginButton = {width: "100%", borderRadius: 0, backgroundColor: "#726fab", borderColor: "#726fab", color: 'white', height: '3em'};
+exports.loginButton = {width: "100%", borderRadius: 0, backgroundColor: "#726fab", borderColor: "#726fab", color: 'white', height: '3em', cursor: "pointer"};
